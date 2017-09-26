7f8f09a959ccf52f899351edf5d08fbfdfd62ab9
jpcastanod <30413807+jpcastanod@users.noreply.github.com>
2017-09-25 22:19:37 -0500

    Create JuanPabloCastañoDuqueTaller8.java
    Aclaro que el ejercicio tres no me funcionó correctamente, pero como el tiempo no me alcanzó decidí mandarlo incompleto.

---
+++ talleres/taller08/JuanPabloCastañoDuqueTaller8.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+/**
+ *
+ * @author 
+ */
+public class Taller8 {
+    
+    //Punto 1
+    public static Stack<Integer> inversa (Stack<Integer> stack){
+        Stack <Integer> aux = new Stack <Integer> ();
+        while (stack.size()>0){
+            aux.push(stack.pop()) ;           
+        }       
+       return aux;
+    }
+    
+    
+    //Punto 2
+    public static void cola (Queue<String> queue){
+        while (queue.size () > 0) {
+        System.out.println ("Atendiendo a: " + queue.poll());
+    }
+    }
+    
+    
+    //notacion polaca
+    public static int polaca  (String string){
+     Stack <Integer> aux = new Stack <Integer> ();
+     Stack <String> aux2 = new Stack <String> ();
+    int answer = 0;
+    int x = 0;
+    String s = "";
+     for (int i = 0; i< string.length (); i++) {
+         if (string.substring (i, i+1)  == "+"|| string.substring (i, i+1)  == "-" || string.substring (i, i+1)  == "*"|| string.substring (i, i+1)  == "/" ){
+            aux2.push (string.substring (i, i+1));
+            }
+            else if ( string.substring (i, i+1)  == " ")
+            {
+                i--;
+            }
+            else {
+            int j = 0;
+     aux.push (Integer.parseInt(string.substring (j, j+1)));
+     j++;
+    
+    
+         if (i%2 == 0) {
+             answer += aux.pop ();
+        }
+    }
+    }
+    return answer;
+}
+}
